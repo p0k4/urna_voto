704ee43a61580f26cb0e55408a9cbe7e3a3974a6
Antonio Martins <tewmartins@gmail.com>
2022-06-18 15:08:09 +0100

    0.0.3

---
+++ etapas.js
@@ -0,0 +1,28 @@
+let etapas = [
+    {
+        titulo: 'ministro',
+        numero: 5,
+        candidatos: [
+            {
+                numero: 35221,
+                nome: 'Germinas',
+                partido: 'pst',
+                fotos:[
+                    {url:'img1.png', legenda:'ministro'}
+                ] 
+            },
+            {
+                numero: 74442,
+                nome: 'Franquelin',
+                partido: 'dst',
+                fotos: [
+                    {url:'img4.png' , legenda: 'vice-ministro'}
+                ]
+            },
+        ]
+    }
+
+
+
+
+]
