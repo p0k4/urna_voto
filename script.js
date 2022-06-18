ab8724bf94b152769b2d112153455f127c0ede72
Antonio Martins <tewmartins@gmail.com>
2022-06-18 22:44:49 +0100

    0.0.4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let SeuVotoPara = document.querySelector('.d-1-1 span');
 let cargo = document.querySelector('.d-1-2 span');
 let descricao = document.querySelector('.d-1-4 span');
-let aviso = document.querySelector('d-2');
+let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros =document.querySelector('.d-1-3');
 
